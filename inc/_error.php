81882817927d9efbde8fd6f3efa5f63d45545825
Jérémy <kcchouette@gmail.com>
2013-11-19 19:53:54 +0100

    update license

--- inc/_error.php
+++ inc/_error.php
@@ -2,19 +2,21 @@
 
 /******************************************************************************/
 /*                                                                            */
-/* Pix : Hébergement d'images                                                 */
-/*         v1.1 - 17082010                                                    */
+/* Picture Host			                                              */
+/*         v1.1.1		                                              */
 /******************************************************************************/
-/*                                                                            */
-/* Auteur:                                                                    */
-/*     - Arthur FERNANDEZ (arthur.fernandez@toile-libre.org)                  */
-/*     - Mickael BLATIERE (mickael@saezlive.net)                              */
-/*                                                                            */
-/* Contributeurs :                                                            */
-/*     - Nicolas VIVET (nizox@toile-libre.org)                                */
-/*                                                                            */
-/* Licence : aGPL                                                             */
-/*                                                                            */
+/*Picture Host is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Affero Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Picture Host is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Affero Public License for more details.
+
+    You should have received a copy of the GNU General Affero Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.     */
 /******************************************************************************/
 
 
